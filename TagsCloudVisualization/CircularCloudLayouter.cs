bd1673479f8977a9f015adff94f80c923cf86672
veronika.vasileva <vvv@skbkontur.ru>
2017-08-10 23:17:02 +0500

    init project

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        private Point center;
+
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            return new Rectangle();
+        }
+    }
+}
